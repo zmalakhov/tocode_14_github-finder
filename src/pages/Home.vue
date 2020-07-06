2edc3d7eeaae82d97695bf080b615d95e9cff1f7
zmalakhov <zm.13.15.zm@gmail.com>
2020-07-06 11:14:26 +0300

    #2 - Search - Поле поиска

--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -2,17 +2,45 @@
     <div class="wrapper-content wrapper-content--fixed">
         <section>
             <div class="container">
-                <h1>home page</h1>
+                <!--search-->
+                <search
+                        :value="search"
+                        placeholder="Type user name"
+                        @search="search = $event"
+                />
+                <button class="btn btnPrimary" @click="getRepos">Search</button>
             </div>
         </section>
     </div>
 </template>
 
 <script>
+    import search from '@/components/Search'
+
     export default {
+        components: {search},
+        data() {
+            return {
+                search: ''
+            }
+        },
+        methods: {
+            getRepos() {
+                console.log(`get user ${this.search} repos`);
+            }
+        }
     }
 </script>
 
-<style scoped>
+<style lang="scss" scoped>
+    .container {
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        flex-direction: column;
+    }
 
+    button {
+        margin-top: 40px;
+    }
 </style>
