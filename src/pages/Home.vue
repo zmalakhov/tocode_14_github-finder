22742c82d2b3b7a7ee84523bf4509bdcb1cc8bd5
zmalakhov <zm.13.15.zm@gmail.com>
2020-07-06 13:41:51 +0300

    #4 - Выводим репозитории

--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -9,6 +9,18 @@
                         @search="search = $event"
                 />
                 <button class="btn btnPrimary" @click="getRepos">Search</button>
+
+                <!--repos__wrapper-->
+                <div class="repos__wrapper" v-if="repos">
+                    <!--repo-item-->
+                    <div class="repos-item" v-for="repo in repos" :key="repo.id">
+                        <div class="repos-info">
+                            <a class="link" :href="repo.html_url" target="_blank">{{ repo.name }}</a>
+                            <span>{{ repo.stargazers_count }}  ⭐</span>
+                        </div>
+                    </div>
+                </div>
+
             </div>
         </section>
     </div>
@@ -22,19 +34,21 @@
         components: {search},
         data() {
             return {
-                search: ''
+                search: '',
+                repos: null
             }
         },
         methods: {
             getRepos() {
                 axios
                     .get(`https://api.github.com/users/${this.search}/repos`)
-                        .then(res => {
-                            console.log(res);
-                        })
-                        .catch(err => {
-                            console.log(err);
-                        })
+                    .then(res => {
+                        // console.log(res);
+                        this.repos = res.data
+                    })
+                    .catch(err => {
+                        console.log(err);
+                    })
 
 
                 //console.log(`get user ${this.search} repos`);
@@ -53,5 +67,19 @@
 
     button {
         margin-top: 40px;
+    }
+
+    .repos__wrapper {
+        width: 400px;
+        margin: 30px 0;
+    }
+
+    .repos-info {
+        display: flex;
+        align-items: center;
+        justify-content: space-between;
+        margin-bottom: 10px;
+        padding: 10px 0;
+        border-bottom: 1px solid #dbdbdb;
     }
 </style>
