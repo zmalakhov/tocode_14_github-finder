770537a143a179c10ab68eb7282c47cbef6e4bf8
zmalakhov <zm.13.15.zm@gmail.com>
2020-07-06 10:38:43 +0300

    #1 - Установка - Делаем одностраничник

---
+++ src/pages/Home.vue
@@ -0,0 +1,18 @@
+<template>
+    <div class="wrapper-content wrapper-content--fixed">
+        <section>
+            <div class="container">
+                <h1>home page</h1>
+            </div>
+        </section>
+    </div>
+</template>
+
+<script>
+    export default {
+    }
+</script>
+
+<style scoped>
+
+</style>
